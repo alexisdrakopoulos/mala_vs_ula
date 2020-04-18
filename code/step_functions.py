ac1fc7ea9a4dd101fa74311f97b1780fa48fe1f8
WhatsMyPurpose <44020749+WhatsMyPurpose@users.noreply.github.com>
2020-04-18 12:08:37 +0100

    Update step_functions.py

--- code/step_functions.py
+++ code/step_functions.py
@@ -18,7 +18,7 @@ def random_walk_metropolis():
 
 
 @jit(nopython=True)
-def metropolis_adjusted_langevin(q0, h, force, beta, step_function):
+def metropolis_adjusted_langevin(q0, h, force, beta, step_function=euler_maruyama_step):
 	"""
 	MALA sampling scheme
 	"""
@@ -38,3 +38,23 @@ def rho(q):
 	"""
 	
         return np.exp(-U(q))
+
+
+@jit(nopython=True)
+def run_simulation(q0, N, h, beta, step_function, force, stepScheme=None):
+   	"""
+	Run sampling simulation
+	"""
+	
+    	q_traj = np.zeros((len(q0),N+1))
+    	q_traj[:,0] = q0
+    	t_traj = np.arange(0, h*N, h)
+     
+    	if stepScheme is None:
+        	for i in range(1, N+1):
+            		q_traj[:,i] = step_function(q_traj[:,i-1], h, force, beta)
+    	else:
+        	for i in range(1, N+1):
+            		q_traj[:,i] = step_function(q_traj[:,i-1], stepScheme(h,i,N), force, beta)
+ 
+    	return q_traj, t_traj
