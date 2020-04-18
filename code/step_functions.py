127822479c8b94969454ece5c824a3977b80213d
WhatsMyPurpose <44020749+WhatsMyPurpose@users.noreply.github.com>
2020-04-18 13:01:29 +0100

    Update step_functions.py

--- code/step_functions.py
+++ code/step_functions.py
@@ -34,34 +34,7 @@ def metropolis_adjusted_langevin(q0, h, force, beta, U):
 @jit(nopython=True)
 def rho(q, U):
 	"""
-	Unnormalized probability distribution
+	Unnormalized probability distribution used for the Metropolis-Hastings step in MALA
 	"""
 	
         return np.exp(-U(q))
-
-
-@jit(nopython=True)
-def run_simulation(q0, N, h, beta, step_function, force, U=None, stepScheme=None):
-   	"""
-	Run sampling simulation
-	
-	Args:
-	    q0: An n-dimensional list of initial starting values.
-	    M: Maximum number of steps.
-	    h: Step size.
-	    beta: Scaling factor?	     
-	    U: The target distribution required for the Metropolis-Hastings step in MALA.
-	"""
-	
-    	q_traj = np.zeros((len(q0),N+1))
-    	q_traj[:,0] = q0
-    	t_traj = np.arange(0, h*N, h)
-     
-    	if stepScheme is None:
-        	for i in range(1, N+1):
-            		q_traj[:,i] = step_function(q_traj[:,i-1], h, force, beta, U)
-    	else:
-        	for i in range(1, N+1):
-            		q_traj[:,i] = step_function(q_traj[:,i-1], stepScheme(h,i,N), force, beta, U)
- 
-    	return q_traj, t_traj
