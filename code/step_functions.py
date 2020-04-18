6d5bd721e61e3552cf5f7de04d8bcc89fbbbc3e2
WhatsMyPurpose <44020749+WhatsMyPurpose@users.noreply.github.com>
2020-04-18 12:22:46 +0100

    Update step_functions.py

--- code/step_functions.py
+++ code/step_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import jit
 
 @jit(nopython=True)
-def euler_maruyama(q, h, force, beta):
+def euler_maruyama(q, h, force, beta, U):
 	"""
 	Euler-Maruyama sampling scheme
 	"""
@@ -18,21 +18,21 @@ def random_walk_metropolis():
 
 
 @jit(nopython=True)
-def metropolis_adjusted_langevin(q0, h, force, beta, step_function=euler_maruyama_step):
+def metropolis_adjusted_langevin(q0, h, force, beta, U, step_function=euler_maruyama):
 	"""
 	MALA sampling scheme
 	"""
 	
 	q_t = step_function(q0, h, force, beta)
 
-    	if np.random.uniform(0,1) < rho(q_t)/rho(q0):
+    	if np.random.uniform(0,1) < rho(q_t, U)/rho(q0, U):
         	return q_t
     
     	return q0
 
 
 @jit(nopython=True)
-def rho(q):
+def rho(q, U):
 	"""
 	Unnormalized probability distribution
 	"""
@@ -41,9 +41,12 @@ def rho(q):
 
 
 @jit(nopython=True)
-def run_simulation(q0, N, h, beta, step_function, force, stepScheme=None):
+def run_simulation(q0, N, h, beta, step_function, force, U=None, stepScheme=None):
    	"""
 	Run sampling simulation
+	
+	Args:
+	    U: The target distribution required for the metropolis hastings step in MALA.
 	"""
 	
     	q_traj = np.zeros((len(q0),N+1))
@@ -52,9 +55,9 @@ def run_simulation(q0, N, h, beta, step_function, force, stepScheme=None):
      
     	if stepScheme is None:
         	for i in range(1, N+1):
-            		q_traj[:,i] = step_function(q_traj[:,i-1], h, force, beta)
+            		q_traj[:,i] = step_function(q_traj[:,i-1], h, force, beta, U)
     	else:
         	for i in range(1, N+1):
-            		q_traj[:,i] = step_function(q_traj[:,i-1], stepScheme(h,i,N), force, beta)
+            		q_traj[:,i] = step_function(q_traj[:,i-1], stepScheme(h,i,N), force, beta, U)
  
     	return q_traj, t_traj
